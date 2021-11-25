5670cce42746485a5db60439184a040a0c490939
Willyam <santoswillyam28@gmail.com>
2021-11-25 20:52:31 -0300

    changes

--- ProjetoControle/server.js
+++ ProjetoControle/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/projeto-controle'));
 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/projeto-controle/'}),
+);
 
-app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080); 
