963b7cf5c95f4a18f5ebf43bca55dadd9007241f
Willyam <santoswillyam28@gmail.com>
2021-11-25 21:05:30 -0300

    change

--- ProjetoControle/server.js
+++ ProjetoControle/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/projeto-controle'));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + "/dist/projeto-controle"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/projeto-controle/'}),
-);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/projeto-controle/index.html"));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080); 
+// Inicia a aplicação pela porta configurada
+app.listen(process.env.PORT || 8080);
